2b424217b10491701a3081ba0457b8c8c59e26cf
Adesola Philip Akintola <APAkintola@lancashire.ac.uk>
2025-12-08 16:29:19 +0000

    Add Vercel frontend to CORS origins

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://techtern-frontend.vercel.app"
+    ],
     credentials: true,
   })
 );
